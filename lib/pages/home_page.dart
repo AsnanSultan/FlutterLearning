de5aa0d476fd9477a2a7bc880b16709a0683e389
Asnan Sultan <ansn.sultan676@gmail.com>
2021-08-12 16:48:56 +0500

    Day6 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_with_git/models/catalog.dart';
 
 import 'drawer.dart';
+import 'items_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dumyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Application"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Wellcom to flutter"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dumyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dumyList[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
---
+++ lib/pages/items_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_with_git/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item})
+      : assert(item != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {},
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}"),
+      ),
+    );
+  }
+}
