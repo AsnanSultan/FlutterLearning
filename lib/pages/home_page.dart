b5612c17bfb33115649b7a2e3ed3219e009fcb72
Asnan Sultan <ansn.sultan676@gmail.com>
2021-08-13 11:27:13 +0500

    Day7 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_with_git/models/catalog.dart';
 
 import 'drawer.dart';
 import 'items_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void initState() {
+    super.initState();
+    loadData();
+    setState(() {});
+  }
+
+  void loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var prodectData = decodedData["products"];
+    CatalogModel.items =
+        List.from(prodectData).map<Item>((item) => Item.fromMap(item)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dumyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Application"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dumyList.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                item: dumyList[index],
-              );
-            }),
+        child: (CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModel.items[index],
+                  );
+                })
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
